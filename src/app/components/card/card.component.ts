befe89719b87819ffc6b83f7ddf97146390eac16
qurriahSam <mugirethe1@gmail.com>
2023-06-12 19:25:01 +0300

    feat: get input from app

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { House } from 'src/app/house';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+  @Input() house: House | null = null;
+}
