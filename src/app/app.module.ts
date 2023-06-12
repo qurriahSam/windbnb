f1f56df9728833a5433bfe066ccdd68c6ada3aba
qurriahSam <mugirethe1@gmail.com>
2023-06-12 19:26:51 +0300

    feat: import matchips module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
@@ -20,6 +22,7 @@ import { HomeComponent } from './components/home/home.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
