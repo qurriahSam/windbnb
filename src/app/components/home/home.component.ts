40b078b47bf064e34c81d3b7c040676b30ec8878
qurriahSam <mugirethe1@gmail.com>
2023-06-12 17:14:50 +0300

    feat: subscribe to house service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { House } from 'src/app/house';
+import { HouseService } from 'src/app/services/house.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  houses: House[] = [];
 
+  constructor(private _house: HouseService) {}
+
+  ngOnInit() {
+    this.getHouses();
+  }
+
+  getHouses() {
+    this._house.getHouses().subscribe((houses) => (this.houses = houses));
+  }
 }
