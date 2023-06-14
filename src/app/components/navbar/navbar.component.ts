da80f2e46cd742e2976c69fcbf317d5b2e9ebe84
qurriahSam <mugirethe1@gmail.com>
2023-06-14 15:59:37 +0300

    feat: trigger filter element on/off

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  hideFilter = true;
+  location = 'Helsinki';
 
+  showFilter() {
+    this.hideFilter = !this.hideFilter;
+  }
 }
