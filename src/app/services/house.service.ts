4b08c993d430f934e19625416fbe59b1782371e3
qurriahSam <mugirethe1@gmail.com>
2023-06-18 19:57:04 +0300

    feat: create a filter observerble

--- src/app/services/house.service.ts
+++ src/app/services/house.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, catchError, filter, map, of, tap } from 'rxjs';
+import { Subject, combineLatest, map, of, tap } from 'rxjs';
 
 import { STAYS } from 'src/stays';
 import { House, SearchDetails } from '../house';
@@ -11,4 +11,25 @@ export class HouseService {
   constructor() {}
 
   houses$ = of(STAYS).pipe(tap((data) => console.log(data)));
+
+  private filterHousesSubject = new Subject<SearchDetails>();
+  filterHousesAction$ = this.filterHousesSubject.asObservable();
+
+  filterHousesFunc(searchDetails: SearchDetails) {
+    this.filterHousesSubject.next(searchDetails);
+  }
+
+  filteredHouses$ = combineLatest([
+    this.houses$,
+    this.filterHousesAction$,
+  ]).pipe(
+    tap((result) => console.log(result)),
+    map(([houses, searchParams]) =>
+      houses.filter(
+        (house) =>
+          house.city === searchParams.city &&
+          house.maxGuests >= searchParams.guests
+      )
+    )
+  );
 }
