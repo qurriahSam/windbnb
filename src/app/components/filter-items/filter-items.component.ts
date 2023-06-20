d60c5c63818393afdcf3929ccf1fe4f4a9ff932b
qurriahSam <mugirethe1@gmail.com>
2023-06-20 21:24:21 +0300

    feat: render filtered items

--- src/app/components/filter-items/filter-items.component.ts
+++ src/app/components/filter-items/filter-items.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HouseService } from 'src/app/services/house.service';
 
 @Component({
   selector: 'app-filter-items',
   templateUrl: './filter-items.component.html',
-  styleUrls: ['./filter-items.component.css']
+  styleUrls: ['./filter-items.component.css'],
 })
 export class FilterItemsComponent {
+  houses$ = this._house.filteredHouses$;
 
+  constructor(private _house: HouseService) {}
 }
