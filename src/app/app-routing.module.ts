ef5c60467317b5bcb6a9ad6d965ce9e22e564039
qurriahSam <mugirethe1@gmail.com>
2023-06-20 21:25:49 +0300

    feat: add route to filterItems component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FilterComponent } from './components/filter/filter.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: FilterComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
